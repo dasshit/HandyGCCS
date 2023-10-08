6493844a328eb6f2b88f5cd5f1daacfa1c0fa382
Валерий Коробов <v.korobov@corp.mail.ru>
2023-10-08 21:14:10 +0300

    Refactoring for ruff linter

--- src/handycon/handhelds/ally_gen1.py
+++ src/handycon/handhelds/ally_gen1.py
@@ -78,13 +78,13 @@ async def process_event(
     if active_keys == [] and handycon.event_queue != []:
         this_button = handycon.event_queue[0]
 
-    if active_keys == [185] \
-            and button_on in [1, 2]:
-        await handycon.emit_now(seed_event, button_a, 1)
-    elif active_keys == [] \
-            and seed_event.code == 185 \
-            and button_on == 0:
-        await handycon.emit_now(seed_event, button_a, 0)
+    # if active_keys == [185] \
+    #         and button_on in [1, 2]:
+    #     await handycon.emit_now(seed_event, button_a, 1)
+    # elif active_keys == [] \
+    #         and seed_event.code == 185 \
+    #         and button_on == 0:
+    #     await handycon.emit_now(seed_event, button_a, 0)
 
     # BUTTON 1 (Default: Screenshot) Paddle + Y
     if active_keys == [184] \
