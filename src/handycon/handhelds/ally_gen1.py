2756d0e95eab61ed48bef7be19a2f8b5f7344a84
Валерий Коробов <v.korobov@corp.mail.ru>
2023-10-09 14:33:49 +0300

    Refactoring for ruff linter

--- src/handycon/handhelds/ally_gen1.py
+++ src/handycon/handhelds/ally_gen1.py
@@ -304,7 +304,7 @@ async def process_event(
     if active_keys == [307] \
             and seed_event.code == 186 \
             and button_on == 1:
-        await handycon.emit_now(seed_event, ["Open Keyboard"], 1)
+        handycon.event_queue.append(["Open Keyboard"])
 
     if active_keys == [187] \
             and button_on in [1, 2]:
